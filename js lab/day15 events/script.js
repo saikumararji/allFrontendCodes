536ce1769f790c98ada20e1a5c62a232e576c76c
saikumararji <saikumararji07@gmail.com>
2025-08-08 12:07:46 +0530

    added a semi colon

--- js lab/day15 events/script.js
+++ js lab/day15 events/script.js
@@ -3,7 +3,7 @@ let colorString = "ABCDEF0123456789"
 function generateColor(){
     let color = "#";
     for(let index = 0;index<6;index++){
-        color+= colorString[Math.floor(Math.random)]
+        color+= colorString[Math.floor(Math.random)];
     }
 }
 
